65b45f6dc2e9881c0324510b17411cab36942a3d
Jianxiu Hou <jianxiuhou9@gmail.com>
2026-05-29 01:15:26 -0400

    Day 3 Unit 3.1-3.3: add Hartmann6 + Ackley, generalize experiment script

--- src/al_benchmark/problems/synthetic.py
+++ src/al_benchmark/problems/synthetic.py
@@ -30,3 +30,55 @@ class Branin(BaseProblem):
 
     def _evaluate(self, x: Tensor) -> Tensor:
         return self._fn(x)
+
+class Hartmann6(BaseProblem):
+    """Hartmann-6 function, a standard 6D BO benchmark.
+
+    Features 6 local minima and 1 global minimum at
+    (0.20169, 0.150011, 0.476874, 0.275332, 0.311652, 0.6573)
+    with value approximately -3.32237.
+
+    BoTorch returns the negated form for maximization.
+    """
+
+    def __init__(self) -> None:
+        from botorch.test_functions import Hartmann
+
+        self.name = "Hartmann6"
+        self.dim = 6
+        self._fn = Hartmann(dim=6, negate=True)
+        self.bounds = self._fn.bounds  # [0, 1]^6
+        # Hartmann-6's known global min is -3.32237 (Frazier 2018 cites it);
+        # negated form has max +3.32237
+        self.optimal_value = 3.32237
+
+    def _evaluate(self, x: Tensor) -> Tensor:
+        return self._fn(x)
+
+
+class Ackley(BaseProblem):
+    """Ackley function in 10D, a high-dimensional multimodal stress test.
+
+    Many local minima in concentric rings around the global minimum at
+    the origin (value = 0). Often used to expose BO's degradation under
+    the curse of dimensionality.
+
+    BoTorch returns the negated form for maximization.
+    """
+
+    def __init__(self, dim: int = 10) -> None:
+        from botorch.test_functions import Ackley
+
+        self.name = f"Ackley{dim}D"
+        self.dim = dim
+        self._fn = Ackley(dim=dim, negate=True)
+        # Default Ackley bounds in BoTorch are [-32.768, 32.768]^d; we narrow to
+        # [-5, 5]^d which is what most BO papers use for benchmarking.
+        self._fn.bounds[0, :] = -5.0
+        self._fn.bounds[1, :] = 5.0
+        self.bounds = self._fn.bounds
+        # Ackley's global min is 0; negated max is 0
+        self.optimal_value = 0.0
+
+    def _evaluate(self, x: Tensor) -> Tensor:
+        return self._fn(x)
